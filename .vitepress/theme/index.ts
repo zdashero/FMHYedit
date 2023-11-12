c4740cdcdfda6b7915fd528a399d2f5c6ee17b6a
taskylizard <75871323+taskylizard@users.noreply.github.com>
2023-11-12 19:56:30 +0530

    PWA

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,12 +1,10 @@
-import { h } from "vue";
 import { type Theme, inBrowser } from "vitepress";
 import DefaultTheme from "vitepress/theme";
-import Sidebar from "./components/SidebarCard.vue";
 import Layout from "./Layout.vue";
 import "./style.css";
 import "uno.css";
 
-// if (inBrowser) import("./pwa");
+if (inBrowser) import("./pwa");
 
 export default {
   extends: DefaultTheme,
