97815b349824cb47b958bb7c49c450a149e41043
taskylizard <75871323+taskylizard@users.noreply.github.com>
2023-11-12 17:57:13 +0530

    Add Switch

--- .vitepress/theme/components/ToggleStarred.vue
+++ .vitepress/theme/components/ToggleStarred.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import Switch from "./Switch.vue";
+
 const toggleStarred = () => document.documentElement.classList.toggle("starred-only");
 </script>
 
 <template>
-  <button class="p-[4px 8px] text-xl i-carbon:star" @click="toggleStarred()" />
+  <Switch @click="toggleStarred()" />
 </template>
 
 <style>
