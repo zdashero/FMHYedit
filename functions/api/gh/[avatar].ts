dae4e25442bd1f1827ff98b8c027efc607b8c66f
taskylizard <75871323+taskylizard@users.noreply.github.com>
2024-03-19 10:47:02 +0000

    fix

--- functions/api/gh/[avatar].ts
+++ functions/api/gh/[avatar].ts
@@ -1,8 +1,21 @@
 export const onRequest: PagesFunction = async (context) => {
   const avatar = `https://github.com/${context.params.avatar}.png`
+  // const avatar = `https://github.com/${a[a.length - 1]}.png`
+  try {
+    const response = await fetch(avatar)
 
-  const req = await fetch(avatar)
-  const res = await req.blob()
+    if (!response.ok) {
+      throw new Error('Error fetching avatar')
+    }
 
-  return res
+    return new Response(response.body, {
+      status: response.status,
+      headers: {
+        'Content-Type': 'image/png',
+        'Cache-Control': 'public, max-age=14400' // Cache for 4 hours
+      }
+    })
+  } catch (error) {
+    return new Response('Error fetching avatar', { status: 500 })
+  }
 }
